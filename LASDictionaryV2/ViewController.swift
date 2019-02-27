9d4cd3ca3d46ad7803b197eb1151d1daded0a891
Jackie <jackienorstrom@aol.com>
2019-02-27 14:45:13 -0500

    Fixing up the storyboard. Added new tableView that displays at the app start, button for dictionary. Functioning search bar next.
    Going to merge into Dev, then branch for search bar.

--- LASDictionaryV2/ViewController.swift
+++ LASDictionaryV2/ViewController.swift
@@ -50,8 +50,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return signs.count
-        return 30
+        return signs.count
+//        return 30
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {

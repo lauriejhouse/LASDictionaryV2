187b9b1214f47733ebc7896ce71d10115ac6eaae
Jackie <jackienorstrom@aol.com>
2019-02-27 09:00:51 -0500

    Initial Commit

---
+++ LASDictionaryV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LASDictionaryV2
+//
+//  Created by Jackie on 2/27/19.
+//  Copyright © 2019 LAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

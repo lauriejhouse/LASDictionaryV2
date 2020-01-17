7c9fe205165492732c90b00ddc478fca3a128466
Unknown <jackienorstrom@aol.com>
2020-01-17 15:27:17 -0500

    Going at it another way for the little quiz.

--- LASDictionaryV2/View Controllers/Quiz/RevealViewController.swift
+++ LASDictionaryV2/View Controllers/Quiz/RevealViewController.swift
@@ -13,162 +13,66 @@ import Firebase
 
 class RevealViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var signView: QuizVideoView!
+
     
+   
     
-    @IBOutlet weak var answer0: UIButton!
-    
-    @IBOutlet weak var answer1: UIButton!
-    
-    @IBOutlet weak var answer2: UIButton!
-    
-    @IBOutlet weak var progressLabel: UILabel!
-    
-    //https://www.seemuapps.com/swift-make-a-trivia-quiz-game
-    
-   struct Question {
-           let question: String
-           let answers: [String]
-           let correctAnswer: Int
-       }
-       
-       
-       var questions: [Question] = [
-           Question(
-               question: "Did you know the sign?",
-               answers: ["Yes", "No", "Needs Practice"],
-               correctAnswer: 0),
-           Question(
-               question: "About?",
-               answers: ["Yes", "No", "Needs Practice"],
-                correctAnswer: 0),
-           Question(
-               question: "Is this correct",
-               answers: ["Yes", "No", "Needs Practice"],
-               correctAnswer: 0)
-       ]
-       
-       var currentQuestion: Question?
-       var currentQuestionPos = 0
-       
-       var noCorrect = 0
-    
-    
-    
-    
-    
-    var signs: Signs?
-    var signCard: SignCard?
     
      override func viewDidLoad() {
           super.viewDidLoad()
         
-        currentQuestion = questions[0]
-        setQuestion()
+        //currentQuestion = questions[0]
+        //setQuestion()
         
-        
-      if let label = titleLabel
-          {
-              //label.text = signs?.signName
-            label.text = signCard?.name
-      }
-          
-          //guard let videoName = signs?.signName.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {return}
-        guard let videoName = signCard?.name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {return}
-          
-          let httpsReference = Storage.storage().reference(forURL: "https://firebasestorage.googleapis.com/v0/b/lasdictionaryv2.appspot.com/o/\(videoName).mov")
-          
-          httpsReference.downloadURL() { url, error in
-              print("URL",url as Any)
-              print("ERROR", error as Any)
-              if let url = url, error == nil {
-                  self.signView.configureForUrl(url)
-                  self.signView.isLoop = true
-                  self.signView.play()
-              }
-          }
+//
+//        guard let videoName = signCard?.name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {return}
+//
+//          let httpsReference = Storage.storage().reference(forURL: "https://firebasestorage.googleapis.com/v0/b/lasdictionaryv2.appspot.com/o/\(videoName).mov")
+
+//
+//        let httpsReference = Storage.storage().reference(forURL: "https://firebasestorage.googleapis.com/v0/b/lasdictionaryv2.appspot.com/o/\(videoName).mov")
+//
+//
+//          httpsReference.downloadURL() { url, error in
+//              print("URL",url as Any)
+//              print("ERROR", error as Any)
+//              if let url = url, error == nil {
+//                  self.signView.configureForUrl(url)
+//                  self.signView.isLoop = true
+//                  self.signView.play()
+//              }
+//          }
           
           
       }
-    @IBAction func dismissPressed(_sender: UIButton) {
-        dismiss(animated: true, completion: nil)
-    }
     
     
+    
+    
+    @IBAction func playLocalVideo(_ sender: Any) {
 
-    
-    @IBAction func submitAnswer0(_ sender: Any) {
-        checkAnswer(idx: 0)
+  
+            guard let path = Bundle.main.path(forResource: "video", ofType: "mov") else {
+                return
+            }
+            let videoURL = NSURL(fileURLWithPath: path)
 
-    }
+            // Create an AVPlayer, passing it the local video url path
+            let player = AVPlayer(url: videoURL as URL)
+            let controller = AVPlayerViewController()
+            controller.player = player
+            present(controller, animated: true) {
+                player.play()
+            }
     
-    @IBAction func submitAnswer1(_ sender: Any) {
-        checkAnswer(idx: 1)
-
-    }
-    
-    @IBAction func submitAnswer2(_ sender: Any) {
-        checkAnswer(idx: 2)
-
-    }
-    @IBAction func submitAnswer3(_ sender: Any) {
-        checkAnswer(idx: 3)
 
     }
-    
-    func checkAnswer(idx: Int) {
-           if(idx == currentQuestion!.correctAnswer) {
-               noCorrect += 1
-           }
-           loadNextQuestion()
-       }
-    
-    func loadNextQuestion() {
-          // Show next question
-          if(currentQuestionPos + 1 < questions.count) {
-              currentQuestionPos += 1
-              currentQuestion = questions[currentQuestionPos]
-              setQuestion()
-          // If there are no more questions show the results
-          } else {
-              performSegue(withIdentifier: "showResults", sender: nil)
-          }
 
-      }
-    
-    
-    // Set labels and buttions for the current question
-       func setQuestion() {
-           titleLabel.text = currentQuestion!.question
-           answer0.setTitle(currentQuestion!.answers[0], for: .normal)
-           answer1.setTitle(currentQuestion!.answers[1], for: .normal)
-           answer2.setTitle(currentQuestion!.answers[2], for: .normal)
-           //answer3.setTitle(currentQuestion!.answers[3], for: .normal)
-           progressLabel.text = "\(currentQuestionPos + 1) / \(questions.count)"
-       }
-       
-       // Set the background as a blue gradient
-       func setGradientBackground() {
-           let colorTop =  UIColor.black.cgColor
-           let colorBottom = UIColor.blue.cgColor
-           
-           let gradientLayer = CAGradientLayer()
-           gradientLayer.colors = [ colorTop, colorBottom]
-           gradientLayer.locations = [ 0.0, 1.0]
-           gradientLayer.frame = self.view.bounds
-           
-           self.view.layer.insertSublayer(gradientLayer, at: 0)
-       }
+
+
        
        // Before we move to the results screen pass the how many we got correct, and the total number of questions
-       override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           if(segue.identifier == "showResults") {
-            let vc = segue.destination as! ResultsViewController
-               vc.noCorrect = noCorrect
-               vc.total = questions.count
-           }
-       }
+   //Crop out the text in the video to make sure they don't know what teh word was. Make a few more buttons for each video? then make text saying 'did you know what the first sign is' or 'what is the first sign' and give a few choices. 
     
     
     

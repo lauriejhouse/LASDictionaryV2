8f28efcab27cd923ecddef4b5208552cc85c8bb6
Unknown <jackienorstrom@aol.com>
2020-02-14 08:59:14 -0500

    trying to re-add split view controller so i can get the 'back' function back. and not just a bunch of swipe up/down screens

--- LASDictionaryV2/AppDelegate.swift
+++ LASDictionaryV2/AppDelegate.swift
@@ -18,11 +18,11 @@ import Instabug
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-//        let splitViewController = window!.rootViewController as! UISplitViewController
-//        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
-//        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
-//        splitViewController.preferredDisplayMode = .allVisible
-//        splitViewController.delegate = self
+        let splitViewController = window!.rootViewController as! UISplitViewController
+        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+        splitViewController.preferredDisplayMode = .allVisible
+        splitViewController.delegate = self
 //
         FirebaseApp.configure()
         Instabug.start(withToken: "0506a75ac941c52e66a45a7c46cb1aa0", invocationEvents: [.shake, .screenshot])
@@ -60,15 +60,15 @@ import Instabug
     }
 
     // MARK: - Split view
-//    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
-//        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-//        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-//        if topAsDetailController.signs == nil {
-//            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-//            return true
-//        }
-//        return false
-//    }
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+        if topAsDetailController.signs == nil {
+            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+            return true
+        }
+        return false
+    }
     
 
 }

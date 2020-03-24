6f74a42d188940e148f31324b12d6b0929654d9e
Unknown <jackienorstrom@aol.com>
2020-03-24 09:34:21 -0400

    got app working. splitting to work on quiz.

--- LASDictionaryV2/View Controllers/Quiz/ResultsViewController.swift
+++ LASDictionaryV2/View Controllers/Quiz/ResultsViewController.swift
@@ -1,60 +1,60 @@
+////
+////  ResultsViewController.swift
+////  LASDictionaryV2
+////
+////  Created by Jackie on 1/9/20.
+////  Copyright © 2020 LAS. All rights reserved.
+////
 //
-//  ResultsViewController.swift
-//  LASDictionaryV2
+//import UIKit
 //
-//  Created by Jackie on 1/9/20.
-//  Copyright © 2020 LAS. All rights reserved.
+//class ResultsViewController: UIViewController {
+//    
+//    @IBOutlet weak var titleLabel: UILabel!
+//    @IBOutlet weak var resultsLabel: UILabel!
+//    
+//    var noCorrect = 0
+//    var total = 0
+//    
+//    
+//    
+//    
 //
-
-import UIKit
-
-class ResultsViewController: UIViewController {
-    
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var resultsLabel: UILabel!
-    
-    var noCorrect = 0
-    var total = 0
-    
-    
-    
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        //set the results
-        resultsLabel.text = "You got \(noCorrect) out of \(total)"
-        
-        //calculate percentage right
-         var percentRight = Double(noCorrect) / Double(total)
-        percentRight *= 100
-        
-            var title = ""
-            if(percentRight < 40) {
-                title = "Not Good"
-            } else if(percentRight < 70) {
-                title = "Almost"
-            } else {
-                title = "Good Job"
-            }
-            titleLabel.text = title
-        }
-
-
-    
-    
-    // Based on the percentage of questions you got right present the user with different message
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        //set the results
+//        resultsLabel.text = "You got \(noCorrect) out of \(total)"
+//        
+//        //calculate percentage right
+//         var percentRight = Double(noCorrect) / Double(total)
+//        percentRight *= 100
+//        
+//            var title = ""
+//            if(percentRight < 40) {
+//                title = "Not Good"
+//            } else if(percentRight < 70) {
+//                title = "Almost"
+//            } else {
+//                title = "Good Job"
+//            }
+//            titleLabel.text = title
+//        }
+//
+//
+//    
+//    
+//    // Based on the percentage of questions you got right present the user with different message
+//    
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
--- LASDictionaryV2/View Controllers/Quiz/SignCardStore.swift
+++ LASDictionaryV2/View Controllers/Quiz/SignCardStore.swift
@@ -1,99 +1,99 @@
+////
+////  SignCardStore.swift
+////  LASDictionaryV2
+////
+////  Created by Jackie on 10/29/19.
+////  Copyright © 2019 LAS. All rights reserved.
+////
 //
-//  SignCardStore.swift
-//  LASDictionaryV2
-//
-//  Created by Jackie on 10/29/19.
-//  Copyright © 2019 LAS. All rights reserved.
-//
-
-import UIKit
-
-
-var signsArray = [Signs]()
-
-struct signCardStore {
-    
-
-    
-    static let defaultSign: [SignCard] = {
-        //return parseJSON()
-        return parsePets()
-    }()
-    
-    
-    private static func parsePets() -> [SignCard] {
-      guard let fileURL = Bundle.main.url(forResource: "Signs", withExtension: "plist") else {
-        return []
-      }
-
-      do {
-        let petData = try Data(contentsOf: fileURL, options: .mappedIfSafe)
-        let pets = try PropertyListDecoder().decode([SignCard].self, from: petData)
-        return pets
-      } catch {
-        print(error)
-        return []
-      }
-    }
-
-    
-//  private static  func parseJSON() -> [SignCard] {
-//
-//            if let url = Bundle.main.url(forResource: "csvjson", withExtension: "json") {
-//            do {
-//                let date = Date()
-//                let data = try Data(contentsOf: url)
-//                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
-//
-//                    (json["results"] as? [[String:Any]])?.forEach { j in
-//                        if let name = j["identifier"] as? String, let id = j["id"] as? Int {
+//import UIKit
 //
 //
-//                            let sign = Signs(name: name, number: id)
-//                            signsArray.append(sign)
-//                        }
-//                    }
+//var signsArray = [Signs]()
 //
-//                }
-//                print("Took", Date().timeIntervalSince(date))
-//            } catch {
-//                print(error.localizedDescription)
+//struct signCardStore {
+//    
 //
-//            }
-//        }
+//    
+//    static let defaultSign: [SignCard] = {
+//        //return parseJSON()
+//        return parsePets()
+//    }()
+//    
+//    
+//    private static func parsePets() -> [SignCard] {
+//      guard let fileURL = Bundle.main.url(forResource: "Signs", withExtension: "plist") else {
+//        return []
+//      }
 //
-//  return []  }
-    
-    
-}
-
-
-
-//    func parseJSON() {
-//
-//            if let url = Bundle.main.url(forResource: "csvjson", withExtension: "json") {
-//            do {
-//                let date = Date()
-//                let data = try Data(contentsOf: url)
-//                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
-//
-//                    (json["results"] as? [[String:Any]])?.forEach { j in
-//                        if let name = j["identifier"] as? String, let id = j["id"] as? Int {
-//
-//
-//                            let sign = Signs(name: name, number: id)
-//                            signsArray.append(sign)
-//                        }
-//                    }
-//
-//                }
-//                print("Took", Date().timeIntervalSince(date))
-//            } catch {
-//                print(error.localizedDescription)
-//            }
-//        }
-//
+//      do {
+//        let petData = try Data(contentsOf: fileURL, options: .mappedIfSafe)
+//        let pets = try PropertyListDecoder().decode([SignCard].self, from: petData)
+//        return pets
+//      } catch {
+//        print(error)
+//        return []
+//      }
 //    }
 //
-//
+//    
+////  private static  func parseJSON() -> [SignCard] {
+////
+////            if let url = Bundle.main.url(forResource: "csvjson", withExtension: "json") {
+////            do {
+////                let date = Date()
+////                let data = try Data(contentsOf: url)
+////                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+////
+////                    (json["results"] as? [[String:Any]])?.forEach { j in
+////                        if let name = j["identifier"] as? String, let id = j["id"] as? Int {
+////
+////
+////                            let sign = Signs(name: name, number: id)
+////                            signsArray.append(sign)
+////                        }
+////                    }
+////
+////                }
+////                print("Took", Date().timeIntervalSince(date))
+////            } catch {
+////                print(error.localizedDescription)
+////
+////            }
+////        }
+////
+////  return []  }
+//    
+//    
 //}
+//
+//
+//
+////    func parseJSON() {
+////
+////            if let url = Bundle.main.url(forResource: "csvjson", withExtension: "json") {
+////            do {
+////                let date = Date()
+////                let data = try Data(contentsOf: url)
+////                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+////
+////                    (json["results"] as? [[String:Any]])?.forEach { j in
+////                        if let name = j["identifier"] as? String, let id = j["id"] as? Int {
+////
+////
+////                            let sign = Signs(name: name, number: id)
+////                            signsArray.append(sign)
+////                        }
+////                    }
+////
+////                }
+////                print("Took", Date().timeIntervalSince(date))
+////            } catch {
+////                print(error.localizedDescription)
+////            }
+////        }
+////
+////    }
+////
+////
+////}
